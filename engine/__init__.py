5860ca676f168733d423e7caeac5ff04993b797c
beedo1627 <ling1330396001@gmail.com>
2026-01-27 22:59:36 +0800

    no message

--- engine/__init__.py
+++ engine/__init__.py
@@ -1,16 +1,10 @@
 """交易引擎模块"""
-from .copy_trading import (
-    CopyTradingConfig,
-    MultiTargetCopyTradingBot,
-)
 from .position_copy_trading import (
     PositionCopyTradingBot,
     TrackingState,
 )
 
 __all__ = [
-    'CopyTradingConfig',
-    'MultiTargetCopyTradingBot',
     'PositionCopyTradingBot',
     'TrackingState',
 ]
